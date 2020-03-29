12adb2a573456e1177a2fc8dd599a4321633cfd0
Bvian16 <55576045+Bvian16@users.noreply.github.com>
2020-03-29 18:09:04 +0530

    Add files via upload

---
+++ factorialin one_line.cpp
@@ -0,0 +1,13 @@
+#include<iostream> 
+
+int factorial(int n) 
+{ 
+	return (n==1 || n==0) ? 1: n * factorial(n - 1); 
+} 
+
+int main() 
+{ 
+	int num = 9; 
+	printf ("Factorial of %d is %d", num, factorial(num)); 
+	return 0; 
+} 
